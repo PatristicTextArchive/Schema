01afeb2359bccb86972bd0e7dcee70b846f8db8d
Annette von Stockhausen <annette@vonstockhausen.org>
2023-06-10 13:13:51 +0200

    Update-Script

--- scripts/update_to_PTA-Schma-v3.bash
+++ scripts/update_to_PTA-Schma-v3.bash
@@ -2,9 +2,7 @@
 for file in ../../pta_data/data/**/**/pta*.xml
 do
   echo Bearbeite "$file"
-  java -jar ../../pta_collator/vendor/saxon9he.jar -xsl:update_to_PTA-Schema-v3.xsl -s:"$file" -o:"$file"_temp -strip:all
-  ~/bin/nodepretty.sh "$file"_temp "$file"_clean
-  cp "$file"_clean "$file"
+  java -jar ~/bin/saxon9he.jar -xsl:update_to_PTA-Schema-v3.xsl -s:"$file" -o:"$file"_temp -strip:all
+  cp "$file"_temp "$file"
   rm "$file"_temp
-  rm "$file"_clean
 done
